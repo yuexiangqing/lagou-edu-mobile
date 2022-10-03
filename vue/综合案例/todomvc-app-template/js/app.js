0c3723fd01b0b9fb4fdb70cb773dc8e45925cf59
yuexiaogou <xiangqing0914@icloud.com>
2022-10-03 10:54:41 +0800

    事项列表展示

--- vue/综合案例/todomvc-app-template/js/app.js
+++ vue/综合案例/todomvc-app-template/js/app.js
@@ -2,5 +2,17 @@
 	'use strict';
 
 	// Your starting point. Enjoy the ride!
+	new Vue({
+		el:'#app',
+		data:{
+			// todos 用于存储所有事项信息
+			todos:[
+				{id:1,title:'示例内容1',completed:true},
+				{id:2,title:'示例内容2',completed:false},
+				{id:3,title:'示例内容3',completed:false},
+			]
+		}
+	})
+	
 
 })(window);
