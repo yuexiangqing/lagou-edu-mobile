71bc93dd464dfcaff093c8afdc70d22e6fed2221
yuexiaogou <xiangqing0914@icloud.com>
2022-10-03 15:14:47 +0800

    待办事项个数 和单位复数

--- vue/综合案例/todomvc-app-template/js/app.js
+++ vue/综合案例/todomvc-app-template/js/app.js
@@ -9,8 +9,26 @@
 			todos:[
 				{id:1,title:'示例内容1',completed:true},
 				{id:2,title:'示例内容2',completed:false},
-				{id:3,title:'示例内容3',completed:false},
+				{id:3,title:'示例内容3',completed:true},
 			]
+		},
+		methods:{
+			//进行单位复数化处理
+			// pluralize:function(){
+			// 	return this.remaining===1?'item':'items'
+			// }
+			pluralize(word){
+				return word + (this.remaining===1?'':'s')
+			}
+		},
+		computed:{
+			// 用于获取待办事项个数
+			remaining(){
+				// return this.todos.filter(todo=>!todo.completed).length;
+				return this.todos.filter(function(todo){
+					return !todo.completed
+				}).length
+			}
 		}
 	})
 	
