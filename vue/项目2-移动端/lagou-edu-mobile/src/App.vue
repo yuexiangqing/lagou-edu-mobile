0e5d7cf9a45797515a1f0d8bef0234452f6f420b
yuexiaogou <xiangqing0914@icloud.com>
2022-11-02 15:34:56 +0800

    搭建项目结构-封装请求模块

--- vue/项目2-移动端/lagou-edu-mobile/src/App.vue
+++ vue/项目2-移动端/lagou-edu-mobile/src/App.vue
@@ -2,6 +2,16 @@
   <!-- 根路由出口 -->
     <router-view/>
 </template>
+<script>
+import request from '@/utils/request'
 
+request({
+  method: 'GET',
+  url: '/front/ad/getAdList'
+})
+export default {
+
+}
+</script>
 <style lang="scss">
 </style>
---
+++ vue/项目2-移动端/lagou-edu-mobile/src/utils/request.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+const request = axios.create({
+  baseURL: 'http://edufront.lagounews.com'
+})
+
+export default request
