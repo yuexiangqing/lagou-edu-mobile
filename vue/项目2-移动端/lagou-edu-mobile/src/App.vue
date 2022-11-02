780f2e545e2ab29745ad52cc74a616cf1689112c
yuexiaogou <xiangqing0914@icloud.com>
2022-11-02 14:39:10 +0800

    搭建项目架构-浏览器适配

--- vue/项目2-移动端/lagou-edu-mobile/src/App.vue
+++ vue/项目2-移动端/lagou-edu-mobile/src/App.vue
@@ -3,9 +3,13 @@
     <!-- <router-view/> -->
     <div>
       <van-button>按钮</van-button>
+      <div class="box">示例元素</div>
     </div>
 </template>
 
 <style lang="scss">
-
+.box {
+  width: 100px;
+  height: 100px;
+}
 </style>
--- vue/项目2-移动端/lagou-edu-mobile/src/main.js
+++ vue/项目2-移动端/lagou-edu-mobile/src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入lib-flexible
+import 'amfe-flexible'
+
 // 引入 vant
 import Vant from 'vant'
 import 'vant/lib/index.css'
