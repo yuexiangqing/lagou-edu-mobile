db77aa216569511332c8ee8605616ca82929b138
yuexiaogou <xiangqing0914@icloud.com>
2022-11-03 13:49:58 +0800

    登录功能-导航布局

--- vue/项目2-移动端/lagou-edu-mobile/src/views/login/index.vue
+++ vue/项目2-移动端/lagou-edu-mobile/src/views/login/index.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="login">
-    登录
+    <!-- 导航栏 -->
+    <van-nav-bar
+        title="登录"
+        left-text="返回"
+        left-arrow
+        @click-left="onClickLeft"
+        />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'LoginIndex'
+  name: 'LoginIndex',
+  methods: {
+    onClickLeft () {
+      this.$router.go(-1)
+    }
+  }
 }
 </script>
 
