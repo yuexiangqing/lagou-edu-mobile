d2ccdd8c861ec22bd3bb2938c8d68b3ecd2093b2
yuexiaogou <xiangqing0914@icloud.com>
2022-11-04 10:05:48 +0800

    学习功能-布局处理

--- vue/项目2-移动端/lagou-edu-mobile/src/views/learn/index.vue
+++ vue/项目2-移动端/lagou-edu-mobile/src/views/learn/index.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="learn">
-     学习页面
+    <!-- 顶部功能 -->
+    <van-nav-bar title="已购课程"/>
+    <!-- 课程列表 -->
+    <!-- 底部导航 -->
+     <layout-footer></layout-footer>
   </div>
 </template>
 
 <script>
+import LayoutFooter from '@/components/LayoutFooter'
 export default {
-  name: 'LearnIndex'
+  name: 'LearnIndex',
+  components: {
+    LayoutFooter
+  }
 }
 </script>
 
