a6617a81fb9a15b266dc6226a4e5f0318d69cad8
yuexiaogou <xiangqing0914@icloud.com>
2022-11-04 14:36:36 +0800

    修改

--- vue/项目2-移动端/lagou-edu-mobile/src/components/LayoutFooter.vue
+++ vue/项目2-移动端/lagou-edu-mobile/src/components/LayoutFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="layout-footer">
     <van-tabbar route>
-      <van-tabbar-item replace to="/course" icon="eye-o">选课</van-tabbar-item>
+      <van-tabbar-item replace to="/" icon="eye-o">选课</van-tabbar-item>
       <van-tabbar-item replace to="/learn" icon="edit">学习</van-tabbar-item>
       <van-tabbar-item replace to="/user" icon="user-o">我</van-tabbar-item>
     </van-tabbar>
